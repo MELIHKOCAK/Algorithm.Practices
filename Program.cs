f6e4c63e8280ba09f206e42fe18d16a31ab77843
Melih Koçak <melih3660k@gmail.com>
2025-09-22 15:00:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithm.Practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Two.Pointers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Two.Pointers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //I used "Two Pointers" algorithm technic for find "goal" varaible values.
+            int[] arr = { 90, 1233, 2023, 3000, 41235, 46351, 55511, 93354, 1000652 };
+            int goal = 57534;
+            int first = 0, last = arr.Length - 1;
+
+            while (true)
+            {
+                if ((arr[first] + arr[last]) == goal)
+                    break;
+                else if ((arr[first] + arr[last]) < goal)
+                    first = first + 1;
+                else
+                    last = last - 1;
+            }
+
+            Console.WriteLine($"Dizinin {first + 1} ve {last + 1} elemanını toplayarak hedefinize ulaşabilirsiniz.");
+        }
+    }
+}
