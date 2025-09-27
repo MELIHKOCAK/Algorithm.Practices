11fd6f7b47362d5bebc486c94777ee88be64710b
Melih Koçak <melih3660k@gmail.com>
2025-09-27 20:12:46 +0300

    I Wrote Selection Sort and I Solved LeetCode First Question

---
+++ Algorithm.Question/LeetCode.Merge.String.Alternately/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LeetCode.Merge.String.Alternately
+{
+    internal class Program
+    {
+        public static string MergeAlternately(string word1, string word2, StringBuilder sb)
+        {
+            bool whoIsLength = word1.Length >= word2.Length;
+
+            if (whoIsLength == true)
+            {
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    if (i > word2.Length - 1)
+                        sb.Append(word1[i].ToString());
+                    else
+                        sb.Append(word1[i].ToString() + word2[i].ToString());
+                }
+            }
+            else
+            {
+                for (int i = 0; i < word2.Length; i++)
+                {
+                    if (i > word1.Length - 1)
+                        sb.Append(word2[i].ToString());
+                    else
+                       sb.Append(word1[i].ToString() + word2[i].ToString());
+                }
+            }
+            return sb.ToString();
+           
+        }
+
+        static void Main(string[] args)
+        {
+            /* 
+              Problem: You are given two strings word1 and word2.
+                       Merge the strings by adding letters in alternating order, 
+                       starting with word1. If a string is longer than the other, 
+                       append the additional letters onto the end of the merged string.
+                       Return the merged string.
+                          Constraints:
+                            1 <= word1.length, word2.length <= 100
+                            word1 and word2 consist of lowercase English letters.
+
+                            Input: word1 = "abc", word2 = "pqr"
+                            Output: "apbqcr"
+                            Explanation: The merged string will be merged as so:
+                            word1:  a   b   c
+                            word2:    p   q   r
+                            merged: a p b q c r
+
+            The problem link: https://leetcode.com/problems/merge-strings-alternately/
+
+            */
+
+            //I solved leetcode first algorithm problem. This solution may not be the best solution for this problem.
+
+            string word1 = "abc", word2 = "pqr", result;
+            StringBuilder sb = new StringBuilder();
+            result = MergeAlternately(word1, word2,sb);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ Selection.Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Selection.Sort
+{
+    internal class Program
+    {
+        //I tried myself write selection sort algorithm. This project may has not best solution for Selection Sort Algorithm.
+        public static void SelectionSort(int[] arr)
+        {
+            int temp;
+            for (int j = 0; j < arr.Length; j++)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[j] < arr[i])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 62, -5, 21, 98, 85, 2, 56, 654, 10, -90, -220 };
+            SelectionSort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
